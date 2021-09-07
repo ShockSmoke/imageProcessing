bec051320336d0357563be6c7126ac84dadbeab3
ShockSmoke <adityagupta0116@gmail.com>
2021-09-07 10:13:49 +0530

    latest

--- imageProcessing.py
+++ imageProcessing.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('test.mp4')
 font = cv2.FONT_HERSHEY_COMPLEX
 
 
@@ -19,8 +19,12 @@ while (1):
     equc = cv2.merge((equc_b, equc_g, equc_r))
     frame=equc
 
+
+
     kernel = np.ones((3, 3), np.uint8)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+
 
     #below values should be corrected according to image
     lower_blue = np.array([110, 50, 50])
@@ -38,32 +42,70 @@ while (1):
     if len(contours) > 0:
 
         areas = [cv2.contourArea(c) for c in contours]
-        print(areas)
-        max_index = np.argmax(areas)
-        cnt = contours[max_index]
-        area= cv2.contourArea(cnt)
 
-    # cv2.drawContours(frame, cnt, -1, (0,0,255), 3)
+
+        #finding max and second max contour
+        mx=0
+        secondmax=0
+        n =len(areas)
+        for i in range(2,n):
+            if areas[i]>mx:
+                secondmax=mx
+                mx=i
+            elif areas[i]>secondmax and \
+                mx != i:
+                secondmax=i
+        cnt = contours[mx]
+        cnt2= contours[secondmax]
+
+
+
+    area= cv2.contourArea(cnt)
 
     # approx the contour a little
     epsilon = 0.0005 * cv2.arcLength(cnt, True)
     approx = cv2.approxPolyDP(cnt, epsilon, True)
 
-    distance = 2*(10**(-7))* (area**2) - (0.0067 * area) + 83.487
+    epsilon = 0.0005 * cv2.arcLength(cnt2, True)
+    approx = cv2.approxPolyDP(cnt2, epsilon, True)
+
+
+
+
+    #finding centre
     M = cv2.moments(cnt)
     Cx = int(M['m10']/M['m00'])
     Cy = int(M['m01'] / M['m00'])
 
+    M2 = cv2.moments(cnt2)
+    Cx2 = int(M2['m10']/M2['m00'])
+    Cy2 = int(M2['m01'] / M2['m00'])
 
+    a= int((Cx + Cx2)/2)
+    b= int((Cy + Cy2)/2)
+
+    
+    cv2.circle(frame, (a,b), 17, (0, 0, 255), -1)
+    
+    
+
+
+    #finding distance
+    distance = 2*(10**(-7))* (area**2) - (0.0067 * area) + 83.487
     S = 'Distance Of Object: ' + str(distance)
     cv2.putText(frame, S, (5, 50), font, 2, (0, 0, 255), 2, cv2.LINE_AA)
+
+
+    #drawing rectangles
     x,y,w,h = cv2.boundingRect(cnt)
     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    x,y,w,h = cv2.boundingRect(cnt2)
+    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+ 
 
-    # cv2.imshow('res', res)
 
     cv2.imshow('frame',frame)
-    print(area)
+
 
     k = cv2.waitKey(5) & 0xFF
 
